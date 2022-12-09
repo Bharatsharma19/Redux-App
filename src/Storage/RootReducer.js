70de10bf22a3850687a442883f9ca7a49086b1f0
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-12-09 08:40:34 +0530

    Updated

--- src/Storage/RootReducer.js
+++ src/Storage/RootReducer.js
@@ -6,7 +6,7 @@ export default function RootReducer(state = initialState, actions) {
   switch (actions.type) {
     case "ADD_EMPLOYEE":
       state.employee[actions.payload[0]] = actions.payload[1];
-      return { state: state.employee };
+      return { employee: state.employee };
 
     default:
       return state;
